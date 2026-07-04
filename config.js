173577f0bd95c0699cc2b41a174693efe6df1021
quangcaoads2025 <quangcaoads2025@gmail.com>
2026-07-04 13:37:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Dán URL Web App Google Apps Script của bạn vào đây sau khi deploy.
 // Ví dụ: https://script.google.com/macros/s/AKfycb.../exec
 window.BYD_CRM_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwRbAxv07mQAPwnBTFwGNupFC_nGQVwMFDajC6SXT5Zpfnb_RYkzKMohcqkDtYvHXxG/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyKhW2W6VTjVVC5s-lJuOESInUH9KSXdZ7GA38SO4o2cddsOFSNh-c8tbizUnwm2iQ/exec",
   APP_NAME: "BYD CRM",
   BRAND_NAME: "Ngọc Anh BYD",
   VERSION: "1.0.0"
