10f3099bd492abec7961857021b677ce49396ea3
quangcaoads2025 <quangcaoads2025@gmail.com>
2026-07-04 15:11:18 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Dán URL Web App Google Apps Script của bạn vào đây sau khi deploy.
-// Ví dụ: https://script.google.com/macros/s/AKfycb.../exec
-window.BYD_CRM_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzo-RZcMkdoW7rfQSYL-xDGMl6BIBJ2WipUZbqi8eHmmeczz-yYRlQ67kMrVeViyp6A/exec",
-  APP_NAME: "BYD CRM",
-  BRAND_NAME: "Ngọc Anh BYD",
-  VERSION: "1.0.0"
-};
