58b2e7b02a090894d4397999309267e320b23503
quangcaoads2025 <quangcaoads2025@gmail.com>
2026-07-04 11:58:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Dán URL Web App Google Apps Script của bạn vào đây sau khi deploy.
+// Ví dụ: https://script.google.com/macros/s/AKfycb.../exec
+window.BYD_CRM_CONFIG = {
+  API_URL: "PASTE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  APP_NAME: "BYD CRM",
+  BRAND_NAME: "Ngọc Anh BYD",
+  VERSION: "1.0.0"
+};
